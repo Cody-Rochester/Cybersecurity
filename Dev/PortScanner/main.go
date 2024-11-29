3c766f7e1da49c05189f59fc241f80fbe2ec27d4
Cody-Rochester <cody.michael.rochester@gmail.com>
2024-11-29 15:05:51 -0600

    add PortScanner

---
+++ Dev/PortScanner/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"PortScanner/port"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("PortScanner in Go")
+
+	open := port.ScanPort("tcp", "localhost", 5500)
+	fmt.Printf("Port Open: %t\n", open)
+
+}
